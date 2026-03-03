8dfdd011864ee186519363a54b36b8674541769f
HubertTom <113830307+Pomolokoki@users.noreply.github.com>
2026-03-03 09:13:16 +0100

    fix: db credentials

--- migrations/migrate.php
+++ migrations/migrate.php
@@ -4,15 +4,15 @@ $migrationsFile = __DIR__ . "/migrations/migrations.json";
 if (file_exists($migrationsFile)) {
 
     try {
-        $p = new PDO("mysql:host=localhost;dbname=ma_bdd;charset=utf8mb4", 'db_user', 'db_pwd');
+        $pdo = new PDO("mysql:host=localhost;dbname=mydb;charset=utf8mb4", 'db_user', 'db_pwd');
 
-        $p->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $p->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     } catch (PDOException $e) {
         echo 'Erreur lors de la connexion à la BDD : ' . $e->getMessage();
         exit();
     }
-    
+
     $migrations = json_decode(file_get_contents($migrationsFile), true);
 
     $pdo->exec("
