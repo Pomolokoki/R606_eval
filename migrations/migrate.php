402b2880cf5ea2ce198512d6f2f69011a829bca6
HubertTom <113830307+Pomolokoki@users.noreply.github.com>
2026-03-03 08:53:39 +0100

    feat: added migration file

---
+++ migrations/migrate.php
@@ -0,0 +1,37 @@
+<?php
+
+$migrationsFile = __DIR__ . "/migrations/migrations.json";
+if (file_exists($migrationsFile)) {
+
+    try {
+        $p = new PDO("mysql:host=localhost;dbname=ma_bdd;charset=utf8mb4", 'db_user', 'db_pwd');
+
+        $p->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $p->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    } catch (PDOException $e) {
+        echo 'Erreur lors de la connexion à la BDD : ' . $e->getMessage();
+        exit();
+    }
+    
+    $migrations = json_decode(file_get_contents($migrationsFile), true);
+
+    $pdo->exec("
+        CREATE TABLE IF NOT EXISTS migrations (
+            id INTEGER PRIMARY KEY,
+            executed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+    ");
+    $executedMigrations = $pdo->query("SELECT id FROM migrations")->fetchAll(PDO::FETCH_COLUMN);
+
+    foreach ($migrations as $migration) {
+        if (!in_array($migration["id"], $executedMigrations)) {
+            $params = [];
+            foreach (get_object_vars($migration) as $key => $value) {
+                $params[":" . $key] = $value;
+            }
+            $pdo->prepare($migration["script"])->execute($params);
+            $pdo->prepare("INSERT INTO migrations (id) VALUES (:id)")->execute([":id" => $migration["id"]]);
+        }
+    }
+}
+?>
