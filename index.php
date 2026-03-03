7198ff9f3dff329d87697358629218afd6b4c771
HubertTom <113830307+Pomolokoki@users.noreply.github.com>
2026-03-03 08:45:02 +0100

    fix: moved database interation on top of file

--- index.php
+++ index.php
@@ -1,3 +1,39 @@
+<?php
+try {
+    $p = new PDO("mysql:host=localhost;dbname=ma_bdd;charset=utf8mb4", 'db_user', 'db_pwd');
+
+    $p->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $p->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    echo 'Erreur lors de la connexion à la BDD : ' . $e->getMessage();
+    exit();
+}
+
+$migrationsFile = __DIR__ . "/migrations/migrations.json";
+if (file_exists($migrationsFile)) {
+    $migrations = json_decode(file_get_contents($migrationsFile), true);
+
+    $pdo->exec("
+        CREATE TABLE IF NOT EXISTS migrations (
+            id INTEGER PRIMARY KEY,
+            executed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+    ");
+    $executedMigrations = $pdo->query("SELECT id FROM migrations")->fetchAll(PDO::FETCH_COLUMN);
+
+    foreach ($migrations as $migration) {
+        if (!in_array($migration["id"], $executedMigrations)) {
+            $params = [];
+            foreach (get_object_vars($migration) as $key => $value) {
+                $params[":" . $key] = $value;
+            }
+            $pdo->prepare($migration["script"])->execute($params);
+            $pdo->prepare("INSERT INTO migrations (id) VALUES (:id)")->execute([":id" => $migration["id"]]);
+        }
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -17,42 +53,6 @@
         <p style="color: crimson; font-weight: bold; border: solid 2px crimson; padding: 5px; width: fit-content;">
             Pensez à inviter cdiiv sur votre projet Github</p>
     </header>
-
-    <?php
-    try {
-        $p = new PDO("mysql:host=localhost;dbname=ma_bdd;charset=utf8mb4", 'db_user', 'db_pwd');
-
-        $p->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $p->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-    } catch (PDOException $e) {
-        echo 'Erreur lors de la connexion à la BDD : ' . $e->getMessage();
-        exit();
-    }
-
-    $migrationsFile = __DIR__ . "/migrations/migrations.json";
-    if (file_exists($migrationsFile)) {
-        $migrations = json_decode(file_get_contents($migrationsFile), true);
-
-        $pdo->exec("
-        CREATE TABLE IF NOT EXISTS migrations (
-            id INTEGER PRIMARY KEY,
-            executed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-        )
-    ");
-        $executedMigrations = $pdo->query("SELECT id FROM migrations")->fetchAll(PDO::FETCH_COLUMN);
-
-        foreach ($migrations as $migration) {
-            if (!in_array($migration["id"], $executedMigrations)) {
-                $params = [];
-                foreach (get_object_vars($migration) as $key => $value) {
-                    $params[":" . $key] = $value;
-                }
-                $pdo->prepare($migration["script"])->execute($params);
-                $pdo->prepare("INSERT INTO migrations (id) VALUES (:id)")->execute([":id" => $migration["id"]]);
-            }
-        }
-    }
-    ?>
 
     <table>
         <thead style="font-weight: bold;">
